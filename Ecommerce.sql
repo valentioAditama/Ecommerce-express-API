8faf660d37f8b23999a23cd2cd5877734ede4186
valentioAditama <valentioaditama8@gmail.com>
2023-09-23 04:37:25 +0700

    initialize first

---
+++ Ecommerce.sql
@@ -0,0 +1,75 @@
+CREATE TABLE `users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` tinyint(1) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `products` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `price` integer(20) NOT NULL,
+  `product_detail` varchar(250) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+);
+
+CREATE TABLE `product_attributes` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `products_id` INT NOT NULL,
+  `products_size` varchar(10) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
+);
+
+CREATE TABLE `product_photos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `products_id` INT NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
+);
+
+CREATE TABLE `cart` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `products_id` INT NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
+);
+
+CREATE TABLE `checkout` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `products_id` INT NOT NULL,
+  `totally` integer(20) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
+);
+
+
+ALTER TABLE `products` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `product_photos` ADD FOREIGN KEY (`products_id`) REFERENCES `products` (`id`);
+
+ALTER TABLE `cart` ADD FOREIGN KEY (`products_id`) REFERENCES `products` (`id`);
+
+ALTER TABLE `product_attributes` ADD FOREIGN KEY (`products_id`) REFERENCES `products` (`id`);
+
+ALTER TABLE `checkout` ADD FOREIGN KEY (`products_id`) REFERENCES `products` (`id`);
