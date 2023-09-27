c2d25bbb20a0f3c7001c5164e05af23c21d87b38
valentioAditama <valentioaditama8@gmail.com>
2023-09-27 09:59:24 +0700

    update jwt still fixing

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var database = require('./src/config/database');
 var usersRouter = require('./src/api/v1/routes/users');
 
@@ -13,6 +12,38 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(usersRouter);
+
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+app.post("/login/generateToken",(req, res) => {
+
+// Access the JWT secret key from the environment variables.
+const secretKey = process.env.JWT_SECRET_KEY;
+
+// Payload data to include in the token.
+const payload = { username: "root" };
+
+// Generate a JWT token with the payload and secret key.
+const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
+
+res.send(token);
+});
+
+app.post("/login/checkJWT",(req, res) => {
+
+    // Access the JWT secret key from the environment variables.
+    const secretKey = process.env.JWT_SECRET_KEY;
+    
+    // Payload data to include in the token.
+    const payload = { username: "root" };
+    
+    // Generate a JWT token with the payload and secret key.
+    const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
+    
+    res.send(token);
+    });
 
 module.exports = app;
--- src/api/v1/routes/users.js
+++ src/api/v1/routes/users.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 const usersController = require('../controllers/users.controller');
 const { usersValidation } = require('../validations/users.validation');
+const authenticateJWT = require('../middleware/auth');
 
-router.get('/api/v1/users', usersController.getAllUsers);
+router.get('/api/v1/users', authenticateJWT, usersController.getAllUsers);
 router.get('/api/v1/users/:id', usersController.findById);
 router.post('/api/v1/users', usersValidation(), usersController.create);
 router.put('/api/v1/users/:id', usersValidation(), usersController.update);
