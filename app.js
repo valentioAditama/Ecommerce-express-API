fdbfed6abe83dd5c7b310f75753d83faf953ce4f
valentioAditama <valentioaditama8@gmail.com>
2023-09-29 23:38:27 +0700

    update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(usersRouter);
 app.use(productRouter);
-app.use(productPhotosRouter);
+app.use(productPhotosRouter); 
 app.use(productAttributesRouter);
 app.use(checkoutRouter);
 app.use(cartRouter);
